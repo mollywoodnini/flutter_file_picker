f1b1003de894e6d2903eed3f7078cda5819966cb
Tan Nghia La <nghiala@live.de>
2021-02-19 13:16:06 +0100

    removed files

---
+++ ios/Classes/HelloWorld.h
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.h
+//  Runner
+//
+//  Created by Tan Nghia La on 19.02.21.
+//
+
+#ifndef HelloWorld_h
+#define HelloWorld_h
+
+
+#endif /* HelloWorld_h */
---
+++ ios/Classes/HelloWorld.m
@@ -0,0 +1,8 @@
+//
+//  HelloWorld.m
+//  Runner
+//
+//  Created by Tan Nghia La on 19.02.21.
+//
+
+#import <Foundation/Foundation.h>
